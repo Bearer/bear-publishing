5e347d7a2205db8cd02366ec97f93f69ca30bc07
Cédric Fabianski <cfabianski@me.com>
2023-01-18 21:38:00 +0100

    feat: add structure.sql

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   require 'jwt'
 
   belongs_to :organization
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   has_many :orders, dependent: :nullify
   passwordless_with :email
+
+  encrypts :email, :name
 end
--- db/structure.sql
+++ db/structure.sql
@@ -1,5 +1,8 @@
-CREATE TABLE public.users (id bigint NOT NULL,
-                                     first_name character varying, created_at timestamp(6) without time zone NOT NULL,
-                                                                                                             updated_at timestamp(6) without time zone NOT NULL,
-                                                                                                                                                       email character varying DEFAULT ''::character varying NOT NULL);
-
+CREATE TABLE public.users (
+  id bigint NOT NULL,
+  encrypted_first_name character varying,
+  created_at timestamp(6) without time zone NOT NULL,
+  updated_at timestamp(6) without time zone NOT NULL,
+  full_name character varying DEFAULT ''::character varying NOT NULL,
+  name character varying DEFAULT ''::character varying NOT NULL
+);
